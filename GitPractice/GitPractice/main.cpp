7d3f6230b7a021384f0f31a503d4aa00ffd33727
Hoson Zhong <hoson_zhong@hotmail.com>
2018-07-19 11:42:15 +1000

    Revert "Merge remote-tracking branch 'origin/master'"
    This reverts commit 023b18eb20f16dd7dbe872cfc25367b90a7a697e.

--- GitPractice/GitPractice/main.cpp
+++ GitPractice/GitPractice/main.cpp
@@ -1,6 +1,56 @@
+#include <iostream>
+#include <string>
+
+std::string name;
+bool login = false;
+char choice;
+
+void logon()
+{
+	system("cls");
+	std::cout << "Please enter your name: ";
+	std::cin >> name;
+	system("cls");
+}
 
 int main() 
 {
+	do
+	{
+		logon();
+		if (name == "Hoson" || name == "Andrew" || name == "Ed" || name == "Sam" || name == "Matt")
+		{
+			std::cout << "Welcome back " << name << "!\n";
+			login = true;
+			std::cout << std::endl;
+			system("Pause");
+		}
+		else
+		{
+			std::cout << "<Error> User not recognised.\n";
+			std::cout << std::endl;
+			std::cout << "Retry login? y/n\n";
+			std::cin >> choice;
+			switch (choice)
+			{
+			case 'y':
+			{
+				login = false;
+				break;
+			}
+			case 'n':
+			{
+				login = true;
+				break;
+			}
+			default:
+			{
+				std::cin >> choice;
+				break;
+			}
+			}
+		}
 
+	} while (!login);
 	return 0;
 }
