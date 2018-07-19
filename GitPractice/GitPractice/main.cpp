0126c115129ef8dbe839759c07a83b17405a0755
Matt Dodd <matt@doddzy.codes>
2018-07-19 10:59:29 +1000

    Added guessing number game

--- GitPractice/GitPractice/main.cpp
+++ GitPractice/GitPractice/main.cpp
@@ -1,7 +1,38 @@
 #include <iostream>
+#include <random>
+#include <ctime>
 
-int main() 
+
+int main()
 {
+	srand((unsigned int)time(nullptr));
+
+
+	int randomValue = rand() % 10 + 1;
+
+	bool foundAnswer = 0;
+	int answer = -1;
+	while (!foundAnswer)
+	{
+		std::cout << "Enter a number: " << std::endl;
+		std::cin >> answer;
+
+		if (answer < randomValue)
+		{
+			std::cout << "Too low, try again!\n";
+		}
+		else if (answer > randomValue)
+		{
+			std::cout << "Too high, try again!\n";
+		}
+		else
+		{
+			foundAnswer = true;
+			std::cout << "Just right!\n";
+
+		}
+	}
+
 	int a = 0;
 	int b = 0;
 
@@ -13,5 +44,7 @@ int main()
 
 	std::cout << "\nA: " << a << "\tB: " << b << "\nIsn't that amazing!!" << std::endl;
 
+
+	system("pause");
 	return 0;
 }
