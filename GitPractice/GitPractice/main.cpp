f0552d6a1219ba267532327f21de8b4a718314d3
Andrew Kenny <andrewckenny97@gmail.com>
2018-07-19 10:50:46 +1000

    gitPractice Changes

--- GitPractice/GitPractice/main.cpp
+++ GitPractice/GitPractice/main.cpp
@@ -1,6 +1,17 @@
+#include <iostream>
 
 int main() 
 {
+	int a = 0;
+	int b = 0;
+
+	std::cout << "Insert two numbers: ";
+	std::cin >> a >> b;
+
+	std::cout << "\nA: " << a << "\tB: " << b << "\nThe numbers will now magically switch places!" << std::endl;
+	std::swap(a, b);
+
+	std::cout << "\nA: " << a << "\tB: " << b << "\nIsn't that amazing!!" << std::endl;
 
 	return 0;
 }
