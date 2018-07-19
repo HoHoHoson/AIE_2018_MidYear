2625d4fcae626f2f22d9f34b87f380ce89d3b19c
Unknown <sammydeedge@hotmail.com>
2018-07-19 10:56:18 +1000

    Added sick Madlib banter functionality

--- GitPractice/GitPractice/main.cpp
+++ GitPractice/GitPractice/main.cpp
@@ -1,6 +1,25 @@
+#include <string>
+#include <iostream>
+
+std::string adj;
+std::string verb;
+std::string noun;
+
+void enter()
+{
+	std::cout << "Enter an adjective";
+	std::cin >> adj;
+
+	std::cout << "Enter a verb";
+	std::cin >> verb;
+
+	std::cout << "Enter a noun";
+	std::cin >> noun;
+}
 
 int main() 
 {
-
-	return 0;
+	enter();
+	std::cout << "The quick, " << adj << " fox " << verb << "s over the lazy " << noun << std::endl;
+	system("pause");
 }
