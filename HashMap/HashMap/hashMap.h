13f67584f7aa89519b2a23e344f1ebf960a8d7c7
Hoson Zhong <hoson_zhong@hotmail.com>
2018-08-16 17:01:33 +1000

    Finished all functions necessary for the assessment
    If I have time I could work on the optional exercises

--- HashMap/HashMap/hashMap.h
+++ HashMap/HashMap/hashMap.h
@@ -19,7 +19,7 @@ static unsigned int calculateHash(const char* data, unsigned int size = 0)
 }
 
 template<typename T>
-unsigned int calculateHash(const T& value)
+static unsigned int calculateHash(const T& value)
 {
 	assert(false && "No hash function for this type");
 	return 0;
@@ -36,7 +36,9 @@ class hashMap
 	};
 
 public:
-	void addValue(keyType key, valueType value)
+
+
+	void addValue(const keyType& key, const valueType& value)
 	{
 		unsigned int hash = calculateHash(key);
 		hash = hash % size;
@@ -46,28 +48,85 @@ public:
 		m_Data[hash].push_back(p);
 	}
 
-private:
-	/*unsigned int calculateHash(keyType key)
+
+	void removeValue(keyType key)
 	{
-		unsigned int hash = 0;
+		unsigned int hash = calculateHash(key);
+		hash = hash % size;
 
-		unsigned char* data = (unsigned char*)&key;
-		unsigned int dataSize = sizeof(keyType);
+		assert(!m_Data[hash].empty() && "Can't find specified key");
 
-		return hash;
-	}*/
+		auto it = m_Data[hash].begin();
+		do
+		{
+			if (it->key == key)
+			{
+				//moves the iterator forward (++i built in) after deleting the object. Therefore, DON'T CALL ++i again or you'll skip some objects!
+				it = m_Data[hash].erase(it);
+
+				std::cout << "\n'" << key << "' has been removed\n";
+			}
+			else
+				++it;
+
+		} while (it != m_Data[hash].end());
+	}
+
+
+	void clearAll()
+	{
+		for (size_t i = 0; i < size; ++i)
+		{
+			if (!m_Data[i].empty())
+				m_Data[i].clear();
+		}
+	}
+
+
+	void display()
+	{
+		bool isEmpty = true;
+
+		std::cout << "\nDISPLAY HASH TABLE\n";
+
+		for (auto i : m_Data)
+			if (!i.empty())
+			{
+				isEmpty = false;
+				for (auto pair : i)
+					std::cout << "{" << pair.key << "} = " << pair.value << std::endl;	
+			}
+		if (isEmpty)
+			std::cout << "Hash table is empty :(\n";
+	}
+
+
+	hashMap operator[](keyType key)
+	{
+		unsigned int hash = calculateHash(key);
+		hash = hash % size;
+
+		for (auto read : this->m_Data[hash])
+		{
+			std::cout << "\n[" << read.key << "] = " << read.value << std::endl;
+		}
+
+		return *this;
+	}
+
+private:
 	std::list<pair> m_Data[size];
 };
 
 
 template<>
-unsigned int calculateHash(const std::string& value)
+static unsigned int calculateHash(const std::string& value)
 {
 	return calculateHash(value.data(), value.size());
 }
 
 template<>
-unsigned int calculateHash(const int& value)
+static unsigned int calculateHash(const int& value)
 {
 	return calculateHash((const char*)&value, sizeof(int));
 }
--- HashMap/HashMap/main.cpp
+++ HashMap/HashMap/main.cpp
@@ -10,7 +10,15 @@ int main()
 	map1.addValue("Matt", 423);
 	map1.addValue("James", 490);
 	map1.addValue("Karley", 630);
+	map1.display();
 
+	map1.removeValue("James");
 
+	map1["Karley"];
+	map1.clearAll();
+	map1.display();
+
+	std::cout << "\n";
+	system("pause");
 	return 0;
 }
